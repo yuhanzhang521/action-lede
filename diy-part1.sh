85f79d4caf328896e74605fe53020e90eca3eada
yuhanzhang521 <52118290+yuhanzhang521@users.noreply.github.com>
2022-04-05 14:05:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,24 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+cat >> feeds.conf.default <<EOF
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git passwall https://github.com/xiaorouji/openwrt-passwall
+EOF
+
+sed -i 's/192.168.1.1/10.10.0.1/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+mkdir ~/openwrt/package/k3
+cd ~/openwrt/package/k3
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git
+git clone https://github.com/lwz322/k3screenctrl_build.git
+cd ~/openwrt
+
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
