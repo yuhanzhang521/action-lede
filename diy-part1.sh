afda993e9e05ac42c6bcfa1abc34afb2f4587bda
yuhanzhang521 <52118290+yuhanzhang521@users.noreply.github.com>
2022-05-02 06:15:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ src-git kenzo https://github.com/kenzok8/openwrt-packages
 src-git passwall https://github.com/xiaorouji/openwrt-passwall
 EOF
 
-sed -i 's/192.168.1.1/10.10.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
